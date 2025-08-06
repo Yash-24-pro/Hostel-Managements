3e66a48eee962313d901ec187b7281b645690519
Yash Tripathi <yashtripathi2400@gmail.com>
2025-08-06 20:08:34 +0530

    Add files via upload

---
+++ CodeForces339A.cpp
@@ -0,0 +1,36 @@
+/*The teacher has written down the sum of multiple numbers. Pupils should calculate the sum. To make the calculation easier, the sum only contains numbers 1, 2 and 3. Still, that isn't enough for Xenia. She is only beginning to count, so she can calculate a sum only if the summands follow in non-decreasing order. For example, she can't calculate sum 1+3+2+1 but she can calculate sums 1+1+2 and 3+3.
+
+You've got the sum that was written on the board. Rearrange the summans and print the sum in such a way that Xenia can calculate the sum.
+
+Input
+The first line contains a non-empty string s — the sum Xenia needs to count. String s contains no spaces. It only contains digits and characters "+". Besides, string s is a correct sum of numbers 1, 2 and 3. String s is at most 100 characters long.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    vector<int> v;
+    for (char c : s)
+    {
+        if (c != '+')
+        {
+            v.push_back(c - '0');
+        }
+    }
+    sort(v.begin(), v.end());
+
+    string res;
+    for (int i = 0; i < v.size(); ++i)
+    {
+        if (i != 0)
+        {
+            cout << "+";
+        }
+        cout << v[i];
+    }
+    cout << endl;
+
+    return 0;
+}
